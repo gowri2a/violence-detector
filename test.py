94ca38120272a0e4f19532e681bb6afa27adc6e0
Gowri A <130265041+gowri2a@users.noreply.github.com>
2024-05-12 16:46:22 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+
+def imagedown(url, folder):
+    try:
+        os.mkdir(os.path.join(os.getcwd(), folder))
+    except:
+        pass
+    os.chdir(os.path.join(os.getcwd(), folder))
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    print(soup.title.text)
+    images = soup.find_all('img')
+    print("All images ",images)
+    for image in images:
+        
+        name = image['alt']
+        link = image['src']
+        with open(name.replace(' ', '-').replace('/', '') + '.jpg', 'wb') as f:
+            im = requests.get(link)
+            f.write(im.content)
+            print('Writing: ', name)
+
+imagedown('https://m.indiamart.com/city/thiruvananthapuram/car-toy.html', 'toycar')
